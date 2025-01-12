e5250380bcface2f3adf9626f2903af162df490c
Nikhil Sharma <nikhilsharma.csgeek@gmail.com>
2025-01-12 11:55:22 +0530

    configuration for Deployment

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 # importing packages 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 # Default Code 
 # It initializes our app 
@@ -16,6 +17,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # creating our db 
 db=SQLAlchemy(app)
+
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>",)
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder,filename)
 
 import routes
 with app.app_context():
--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://127.0.0.1:5000/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api"
